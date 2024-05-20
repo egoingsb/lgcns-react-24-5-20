d02907e54fe948061fef9d4cdca9efd69623d37a
egoinig <egoing@gmail.com>
2024-05-20 11:04:05 +0900

    props는 html계의 속성이다

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import './App.css'
 
-function Counter(){
+function Counter(props){
+  console.log('props', props);
+  const title = props.title;
+  const initValue = props.initValue;
   return <>
-    <h1>카운터</h1>
-    <button>+</button> 0
+    <h1>{title}</h1>
+    <button>+</button> {initValue}
   </>
 }
 
 function App() {
   return (
     <>
-      <Counter></Counter>
+      <Counter title="카운터" initValue="10"></Counter>
+      <Counter title="불면증 카운터" initValue="20"></Counter>
     </>
   )
 }
